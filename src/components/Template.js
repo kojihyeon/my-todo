100653a4dbc130d7e0c5761c1b7eef582b804498
Ji hyeon Ko <ko339342@naver.com>
2023-08-03 20:11:10 +0900

    react 배포 작업

--- src/components/Template.js
+++ src/components/Template.js
@@ -4,7 +4,7 @@ import "./Template.css";
 const Template = ({ children, todolength }) => {
   return (
     <div className="Template">
-      <div className="title">Todo List ({todolength})</div>
+      <div className="title">Todo List <span className="todolength">{todolength}</span></div>
       <div>{children}</div>
     </div>
   )
